8ec7c53f58667acc5871f1d80107dcbbcd7504aa
Unknown <unknown@example.com>
2018-02-23 18:44:19 +0100

    Reimplemented prototype using Websocket-sharp

--- MopidyReport/Program.cs
+++ MopidyReport/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.WebSockets;
+//using System.Net.WebSockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using WebSocketSharp;
 
 namespace MopidyReport
 {
@@ -12,45 +15,141 @@ namespace MopidyReport
     {
         static void Main(string[] args)
         {
-            var clientTask = Client();
-            clientTask.Wait();
+            Console.OutputEncoding = Encoding.UTF8;
+            //var clientTask = Client();
+            //clientTask.Wait();
             // Console.ReadLine();
-        }
 
-        static async Task Client()
-        {
-            using (ClientWebSocket ws = new ClientWebSocket())
+            WebSocket client = new WebSocket("ws://192.168.178.13:6680/mopidy/ws");
+            client.OnOpen += Client_OnOpen;
+            client.OnError += Client_OnError;
+            client.OnMessage += Client_OnMessage;
+            client.OnClose += Client_OnClose;
+
+            int msgID = 0;
+            client.Connect();
+            try
             {
-                Uri serverUri = new Uri("ws://192.168.178.13:6680/mopidy/ws");
-                await ws.ConnectAsync(serverUri, CancellationToken.None);
                 while (true)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("Input message ('exit' to exit): ");
                     Console.ForegroundColor = ConsoleColor.White;
                     string msg = Console.ReadLine();
-                    Console.ForegroundColor = ConsoleColor.Gray;
                     if (msg == "exit")
-                    {
                         break;
                     }
-                    ArraySegment<byte> bytesToSend = new ArraySegment<byte>(
-                        Encoding.UTF8.GetBytes(msg));
-                    await ws.SendAsync(
-                        bytesToSend, WebSocketMessageType.Text,
-                        true, CancellationToken.None);
-                    ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
-                    WebSocketReceiveResult result = await ws.ReceiveAsync(
-                        bytesReceived, CancellationToken.None);
-                    Console.WriteLine(Encoding.UTF8.GetString(
-                        bytesReceived.Array, 0, result.Count));
-                    if (ws.State != WebSocketState.Open)
-                    {
-                        break;
-                    }
+                    client.Send(msg);
                 }
             }
-
+            finally
+            {
+                client.Close();
+            }
         }
+
+        private static ConsoleColor PrefixTime(ConsoleColor color)
+        {
+            var OldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss.fff") + " ");
+            Console.ForegroundColor = color;
+            return OldColor;
+        }
+
+        private static void Client_OnClose(object sender, CloseEventArgs e)
+        {
+            var OldColor = PrefixTime(ConsoleColor.Cyan);
+            Console.WriteLine("Connection closed");
+            Console.ForegroundColor = OldColor;
+        }
+
+        private static void Client_OnMessage(object sender, MessageEventArgs e)
+        {
+            var OldColor = PrefixTime(ConsoleColor.DarkGray);
+            try
+            {
+                Console.WriteLine(e.Data);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                dynamic data = JsonConvert.DeserializeObject(e.Data);
+                if (data.result != null)
+                {
+                    string result;
+                    if (data.result.GetType() == typeof(string))
+                    {
+                        result = data.result;
+                    }
+                    else
+                    {
+                        result = JsonConvert.SerializeObject(data.result, Formatting.Indented);
+                    }
+                    Console.WriteLine(result);
+                }
+                else if (data.error != null)
+                {
+                    throw new Exception(JsonConvert.SerializeObject(data.error, Formatting.Indented));
+                }
+                else
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Error.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ForegroundColor = OldColor;
+            }
+        }
+
+        private static void Client_OnError(object sender, ErrorEventArgs e)
+        {
+            var OldColor = PrefixTime(ConsoleColor.Red);
+            Console.Error.WriteLine(e.Message);
+            Console.ForegroundColor = OldColor;
+        }
+
+        private static void Client_OnOpen(object sender, EventArgs e)
+        {
+            var OldColor = PrefixTime(ConsoleColor.Cyan);
+            Console.WriteLine("Connection open");
+            Console.ForegroundColor = OldColor;
+        }
+
+        //static async Task Client()
+        //{
+        //    using (ClientWebSocket ws = new ClientWebSocket())
+        //    {
+        //        Uri serverUri = new Uri("ws://192.168.178.13:6680/mopidy/ws");
+        //        await ws.ConnectAsync(serverUri, CancellationToken.None);
+        //        while (true)
+        //        {
+        //            Console.ForegroundColor = ConsoleColor.Green;
+        //            Console.Write("Input message ('exit' to exit): ");
+        //            Console.ForegroundColor = ConsoleColor.White;
+        //            string msg = Console.ReadLine();
+        //            Console.ForegroundColor = ConsoleColor.Gray;
+        //            if (msg == "exit")
+        //            {
+        //                break;
+        //            }
+        //            ArraySegment<byte> bytesToSend = new ArraySegment<byte>(
+        //                Encoding.UTF8.GetBytes(msg));
+        //            await ws.SendAsync(
+        //                bytesToSend, WebSocketMessageType.Text,
+        //                true, CancellationToken.None);
+        //            ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+        //            WebSocketReceiveResult result = await ws.ReceiveAsync(
+        //                bytesReceived, CancellationToken.None);
+        //            Console.WriteLine(Encoding.UTF8.GetString(
+        //                bytesReceived.Array, 0, result.Count));
+        //            if (ws.State != WebSocketState.Open)
+        //            {
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
