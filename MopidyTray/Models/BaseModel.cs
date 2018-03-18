bc1a2ca14f6518f3fd4923597fa6229b3d87eb6b
Martijn Coppoolse <github.com@martijn.coppoolse.com>
2018-03-18 16:40:49 +0100

    Conversion from/to Python date/time ints is always UTC.

--- MopidyTray/Models/BaseModel.cs
+++ MopidyTray/Models/BaseModel.cs
@@ -7,31 +7,35 @@ using System.Threading.Tasks;
 
 namespace MopidyTray.Models
 {
-    class BaseModel
+    public class BaseModel
     {
         [JsonProperty(PropertyName = "__model__")]
         public string Model {
             get
             {
-                return this.GetType().Name.ToLowerInvariant();
+                return this.GetType().Name;
             }
             set
             {
                 if (!value.Equals(this.Model, StringComparison.InvariantCultureIgnoreCase))
-                    throw new NotSupportedException($"Model {value} does not match type {this.GetType().Name}");
+                    throw new NotSupportedException($"Model {value} does not match type {this.Model}");
             }
         }
 
 
-        protected static class Utils
+        public static class Utils
         {
             private const long UnixEpochStart = 621355968000000000; // new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
-                                                                    
-            // TODO: test if this gives the correct date/time (both ways)
-            public static DateTime IntToDateTime(long pythonDateTime) =>
-                new DateTime(UnixEpochStart + pythonDateTime, DateTimeKind.Utc);
-            public static long DateTimeToInt(DateTime dateTime) =>
-                dateTime.ToUniversalTime().Ticks - UnixEpochStart;
+
+            public static DateTime IntToDateTime(long pythonDateTime)
+            {
+                return new DateTime(UnixEpochStart + pythonDateTime, DateTimeKind.Utc);
+            }
+
+            public static long DateTimeToInt(DateTime dateTime)
+            {
+                return dateTime.ToUniversalTime().Ticks - UnixEpochStart;
+            }
         }
     }
 
